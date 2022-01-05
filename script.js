6eb3853dced0b1b5c8c3e0c70d6356f3e2e1c0f5
ChesterWOV <82011969+ChesterChowWOV@users.noreply.github.com>
2022-01-05 19:05:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ meanings = {
   "cenz":["sense", "engwish"],
   "keti":["get it", "keti => geti => get it"],
   "kreem":["go", "kreem => cream => 糕 => go"],
-  "wonx2": ["ok", "wonx2 -> onex2 =-> one one -> eleven -> (eleventh alphabet) -> k -> ok"],
+  "wonx2": ["ok", "wonx2 -> onex2 -> one one -> eleven -> (eleventh alphabet) -> k -> ok"],
   "twentify": ["why", "twentify -> twenty-five -> (twenty-fifth alphabet) -> y -> why"],
 }
 
