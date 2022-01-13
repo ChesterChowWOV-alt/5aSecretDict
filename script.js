5c9a64a81ab53d4c2b10527923bee3188a54f2fd
ChesterWOV <82011969+ChesterChowWOV@users.noreply.github.com>
2022-01-13 16:02:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ document.getElementById("search").addEventListener("click", () => {
 
 
 document.body.style.visibility = "hidden"
-document.body.innerHTML += "<div style=\"width: 99%; margin: 0 auto;\"><input id=login><button id=login-btn>Login</button></div>"
+document.body.innerHTML += "<div style=\"display: flex; align-items: center; justify-content: center;\"><input id=login><button id=login-btn>Login</button></div>"
 let l = document.getElementById("login")
 l.style.visibility = "visible"
 let b = document.getElementById("login-btn")
