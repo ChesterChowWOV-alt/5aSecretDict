18deeafb5e30538b8c91e21f7735920b6f8d8961
ChesterWOV <82011969+ChesterChowWOV@users.noreply.github.com>
2022-01-13 16:19:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ let b = document.getElementById("login-btn")
 b.style.visibility = "visible"
 let w = document.getElementById("welcome")
 w.style.visibility = "visible"
-w.style.transform = "translateY(-50%)"
+w.style.position = "top"
 b.addEventListener("click", () => {
   let val = document.getElementById("login").value
   if (val.toLowerCase() == "hnpok") {
